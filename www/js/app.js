23324d8a65a7dee0b624872d0f64cac17eb9480c
Anthony Bui <anthonybui@gmail.com>
2013-02-23 11:49:14 -0800

    :hearts: added/got the camera picture upload working; also re-indented some files which im pretty sure you guys will yell at me fr in a bit when you see merge conflicts! :gun: :mahjong:

--- www/js/app.js
+++ www/js/app.js
@@ -1,95 +1,102 @@
 (function() {
-	window.footprint = {
-		models: {},
-		views: {},
-		utils: {},
-	};
+  window.footprint = {
+    models: {},
+    views: {},
+    utils: {},
+  };
 
-	Parse.View.prototype.close = function() {
-		if (this.beforeClose) {
-			this.beforeClose();
-		}
-		this.undelegateEvents();
-		this.remove(); 
-	};
-	footprint.Router = Parse.Router.extend({
+  Parse.View.prototype.close = function() {
+    if (this.beforeClose) {
+      this.beforeClose();
+    }
+    this.undelegateEvents();
+    this.remove(); 
+  };
+  footprint.Router = Parse.Router.extend({
 
-		routes: {
-			'': 			"login", 
-		},
+    routes: {
+      '': 			"login", 
+    },
 
-		initialize: function() {
-			var _this = this;
-			this.pageHistory = [];
+    initialize: function() {
+      var _this = this;
+      this.pageHistory = [];
 
-			// $('#content').on('click', '.header-back-button', function(event) {
-			// 	window.history.back();
-			// 	return false;
-			// });
-			// if (document.documentElement.hasOwnProperty('ontouchstart')) {
-			// 	document.addEventListener('touchmove', function(event) {
-			// 		event.preventDefault();
-			// 		return false;
-			// 	});
-			// 	$('#content').on('touchstart','a',function(event) {
-			// 		_this.selectItem(event);
-			// 	});
-			// 	$('#content').on('touchend','a', function(event) {
-			// 		_this.deselectItem(event);
-			// 	})
-			// } else {
-			// 	$('#content').on('mousedown','a', function(event) {
-			// 		this.selectItem(event);
-			// 	});
-			// 	$('#content').on('mousedown','a', function(event) {
-			// 		this.deselectItem(event);
-			// 	} )
-			// }
-			var loginView = new footprint.views.LoginView();
-			loginView.render();
-			$('#content').append(loginView.el);
-		},
+      // $('#content').on('click', '.header-back-button', function(event) {
+      // 	window.history.back();
+      // 	return false;
+      // });
+      // if (document.documentElement.hasOwnProperty('ontouchstart')) {
+      // 	document.addEventListener('touchmove', function(event) {
+      // 		event.preventDefault();
+      // 		return false;
+      // 	});
+      // 	$('#content').on('touchstart','a',function(event) {
+      // 		_this.selectItem(event);
+      // 	});
+      // 	$('#content').on('touchend','a', function(event) {
+      // 		_this.deselectItem(event);
+      // 	})
+      // } else {
+      // 	$('#content').on('mousedown','a', function(event) {
+      // 		this.selectItem(event);
+      // 	});
+      // 	$('#content').on('mousedown','a', function(event) {
+      // 		this.deselectItem(event);
+      // 	} )
+      // }
+      var loginView = new footprint.views.LoginView();
+      var messageCreateView = new footprint.views.MessageCreateView();
+      messageCreateView.render();
+      $('#content').append(messageCreateView.el);
+//      loginView.render();
+//      $('#content').append(loginView.el);
+    },
 
-		selectItem: function(event) {
-			$(event.target).addClass('tappable-active');
-		},
+    selectItem: function(event) {
+      $(event.target).addClass('tappable-active');
+    },
 
-		deselectItem: function(event) {
-			$(event.target).removeClass('tappable-active');
-		},
+    deselectItem: function(event) {
+      $(event.target).removeClass('tappable-active');
+    },
 
-		login: function() {
-			slidePage(new footprint.views.LoginView().render());
-		},
+    login: function() {
+      slidePage(new footprint.views.LoginView().render());
+    },
 
-		slidePage: function(page) {
-			var _this = this;
-			if (!this.currentPage){
-				$(page.el).attr('class','page stage-center');
-				$('#content').append(page.el);
-				this.pageHistory = [window.location.hash];
-				this.currentPage = page;
-			}
-			if (this.currentPage !== this.dashboardPage) {
-				this.currentPage.close();
-			}
-			$('.stage-right .stage-left').not('#dashboardPage').remove(); // probably not needed
-			if (page === this.dashboardPage) {
-				$(page.el).attr('class', 'page stage-left');
-				this.pageHistory = [window.location.hash];
-			} else if (this.pageHistory.length > 1 && window.location.hash === this.pageHistory[this.pageHistory-2]) {
-				$(page.el).attr('class', 'page stage-left');
-				this.pageHistory.pop();
-			} else {
-				$(page.el).attr('class', 'page stage-right');
-				this.pageHistory.push(window.location.hash);
-			}
-			$('#content').append(page.el);
-			setTimeout(function(){
-				$(_this.currentPage.el).attr('class', 'page transition stage-right'); // decides which direction slide either left or right
-				$(page.el).attr('class', 'page stage-center transition');
-				_this.currentPage = page;
-			});
-		},
-	})
+    messageDetail: function() {
+      new footprint.views.MessageCreateView();
+    },
+
+    slidePage: function(page) {
+      var _this = this;
+      if (!this.currentPage){
+	$(page.el).attr('class','page stage-center');
+	$('#content').append(page.el);
+	this.pageHistory = [window.location.hash];
+	this.currentPage = page;
+      }
+      if (this.currentPage !== this.dashboardPage) {
+	this.currentPage.close();
+      }
+      $('.stage-right .stage-left').not('#dashboardPage').remove(); // probably not needed
+      if (page === this.dashboardPage) {
+	$(page.el).attr('class', 'page stage-left');
+	this.pageHistory = [window.location.hash];
+      } else if (this.pageHistory.length > 1 && window.location.hash === this.pageHistory[this.pageHistory-2]) {
+	$(page.el).attr('class', 'page stage-left');
+	this.pageHistory.pop();
+      } else {
+	$(page.el).attr('class', 'page stage-right');
+	this.pageHistory.push(window.location.hash);
+      }
+      $('#content').append(page.el);
+      setTimeout(function(){
+	$(_this.currentPage.el).attr('class', 'page transition stage-right'); // decides which direction slide either left or right
+	$(page.el).attr('class', 'page stage-center transition');
+	_this.currentPage = page;
+      });
+    },
+  })
 }).call(this)
--- www/js/login.js
+++ www/js/login.js
@@ -1,15 +1,52 @@
 (function(){
-	footprint.views.LoginView = Parse.View.extend({
+  footprint.views.LoginView = Parse.View.extend({
 
-		initialize: function() {
-			this.template = Handlebars.compile(footprint.utils.templateLoader.get('login'));
-		},
+    initialize: function() {
+      this.template = Handlebars.compile(footprint.utils.templateLoader.get('login'));
+    },
 
-		render: function() {
-			$(this.el).append(this.template());
-			return this;
-		},
+    render: function() {
+      $(this.el).append(this.template());
+      return this;
+    },
+
+  });
+
+  footprint.views.MessageCreateView = Parse.View.extend({
+    
+    events: {
+      'click #upload_picture': 'capturePhoto'
+    },
+
+    initialize: function() {
+//      this.model.on('change', this.render, this);
+//      this.model.on('destroy', this.render, this);
+      this.template = Handlebars.compile(footprint.utils.templateLoader.get('messageCreate'));
+    },
+    
+    render: function () {
+      $(this.el).append(this.template({ 'content': 'this content is json dynamic!', 'id': '1', 'notes': 'notes' }));
+      return this;
+    },
+
+    capturePhoto: function() {
+      navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+           destinationType: Camera.DestinationType.DATA_URL}); 
+
+      function onSuccess(imageData) {
+        var image = document.getElementById('my_picture');
+        image.src = "data:image/jpeg;base64," + imageData;
+      }
 
 
-	})
+      function onFail(message) {
+        alert('Failed because: ' + message);
+      }
+      
+      alert('finally making some progress');
+      return false;
+    }
+
+    
+  });
 }).call(this)
